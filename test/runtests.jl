f0d498935319bec5f4cacd3dd5d940938bd198a1
tan <tanmaykm@gmail.com>
2019-10-17 11:36:13 +0530

    allow narrower types for smaller models
    Parameterize LSH and allow narrower integer types to be used.

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,14 +3,26 @@ using LSH
 using DelimitedFiles
 using SparseArrays
 
-# testinput.csv prepared from document-term matrix of few SPDX files:
-# BSD-1-Clause.txt, GPL-1.0-only.txt, LGPL-2.0-only.txt, MIT.txt, Vim.txt
-dtm = sparse(convert(Array{Int64,2}, readdlm("testinput.csv", ',')))
-model = LSHModel(20, dtm)
+function test_typed(T)
+    @info("testing with type $T")
 
-@info("testing signatures...")
-signatures = signature(model, dtm)
-@test signatures == model.dochashes
+    # testinput.csv prepared from document-term matrix of few SPDX files:
+    # BSD-1-Clause.txt, GPL-1.0-only.txt, LGPL-2.0-only.txt, MIT.txt, Vim.txt
+    dtm = convert(SparseMatrixCSC{T,T}, sparse(convert(Array{T,2}, readdlm("testinput.csv", ','))))
+    model = LSHModel(20, dtm)
 
-nfailures = LSH.selftest(model)
-@test nfailures == 0
+    @info("testing signatures...")
+    signatures = signature(model, dtm)
+    @test signatures == model.dochashes
+
+    nfailures = LSH.selftest(model)
+    @test nfailures == 0
+
+    allmatches = LSH.match(model, model.dochashes)
+    for idx in 1:length(allmatches)
+        @test first(allmatches[idx]).id == idx
+    end
+end
+
+test_typed(Int64)
+test_typed(Int32)
